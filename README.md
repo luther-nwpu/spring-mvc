a9e67aca3073029f1ea01588917647f472e0fd07
张露 <2424733678@qq.com>
2018-09-29 11:54:45 +0800

    finish mvc

---
+++ README.md
@@ -0,0 +1,3 @@
+#Helloworld 
+helloworld 见 https://www.cnblogs.com/wormday/p/8435617.html
+spring mvc
---
+++ src/com/helloworld/HiController.java
@@ -0,0 +1,17 @@
+package com.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; // 这里导入了一个Model类
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hi")
+public class HiController {
+
+    @RequestMapping("/say")
+    public String say(Model model) { // 参数中传入Model
+        model.addAttribute("name","wormday"); // 指定Model的值
+        model.addAttribute("url","http://www.cnblogs.com/wormday/p/8435617.html"); // 指定Model的值
+        return "say";
+    }
+}
